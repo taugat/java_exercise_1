7eec96817325b867964bfd52fe028b9a054ad4d2
Auguste Hatton <auguste.hatton@gmail.com>
2021-05-19 17:18:20 +0200

    kjghk

--- src/Launcher.java
+++ src/Launcher.java
@@ -15,19 +15,18 @@ public class Launcher {
         Collection<Command> commands = Arrays.asList(new Fibo(), new Freq(), new Quit());
 
         try (Scanner scanner = new Scanner(System.in)) {
-            while (scanner.hasNext()) {
 
-                try {
-                    commands.stream()
-                            .filter(command -> command.name().equals(scanner.nextLine()))
-                            .findAny().orElseThrow(() -> new Exception("Unknown command"))
-                            .run(scanner);
-                }
-                catch (Exception e)
-                {
-                    System.err.println(e.getMessage());
-                }
+
+            while (true) {
+                String commandString = scanner.nextLine();
+                commands.stream()
+                        .filter(command -> command.name().equals(commandString))
+                        .findAny()
+                        .orElseThrow(() -> new Exception("Unknown command"))
+                        .run(scanner);
             }
+
+
         } catch (Exception e) {
             System.err.println("Error : " + e.getMessage());
         }
